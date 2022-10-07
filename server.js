6625d567cb8796475e030bfbe955355bca212f95
Serverror7 <shrimpfeed@gmail.com>
2022-10-07 13:16:20 -0600

    Work done as of 10/7

--- server.js
+++ server.js
@@ -25,25 +25,33 @@ app.get("/", (req,res)=>{
     res.send("Hello World!")
 });
 
-app.post('/user', (req,res)=>{
+app.post('/user', async (req,res)=>{
     const newUserRequestObject = req.body;
     console.log('New User:',JSON.stringify(newUserRequestObject));
+    newUserRequestObject.password = md5(newUserRequestObject.password);
     redisClient.hSet('users',req.body.email,JSON.stringify(newUserRequestObject));
     res.send('New User'+newUserRequestObject.email+'added');
 });
 
-app.post("/login", (req,res)=>{
+app.post("/login", async (req,res)=>{
+    
     const loginEmail = req.body.userName;
     console.log(JSON.stringify(req.body));
     console.log("loginEmail", loginEmail);
     const loginPassword = req.body.password;
     console.log("loginPassword", loginPassword);
-    res.send("Who are you");
-
-    if (loginEmail == "mar22043@byui.edu" && loginPassword == "st6@nG"){
+    const userString = await redisClient.hGet("users", loginEmail);
+    const userObject = JSON.parse(userString);
+    if (userString == "" || userString == null){
+        res.status(404);
+        res.send("User not found");
+    }
+    else if (md5(loginPassword) == userObject.password){
         const token = uuidv4();
+        res.send(token)
+        // if there is no status change it is all ok or 200
     } else{
         res.status(401);
         res.send("Invalid user or password");
     }    
-})
+});
